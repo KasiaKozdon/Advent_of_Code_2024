69e9b7ae387d3e9ea8780c356dacc7b7ae33fbc4
KasiaKozdon <katarzyna.kozdon@gmail.com>
2024-12-14 11:11:36 +0100

    Day 4, part 2

--- Day4.py
+++ Day4.py
@@ -7,15 +7,47 @@ def generate_answer(input: str) -> int:
     xmas_instances = 0
     for row_idx, row in enumerate(input):
         for letter_idx, letter in enumerate(row):
-            for x_stride in [-1, 0, 1]:
-                for y_stride in [-1, 0, 1]:
-                    if letter == "X":
-                        xmas_instances += find_xmas_string(input,
-                        x_stride = x_stride, y_stride = y_stride, 
-                        starting_x = letter_idx, starting_y = row_idx,
-                        str_to_find = "XMAS")
+            if letter == "X":
+                for x_stride in [-1, 0, 1]:
+                    for y_stride in [-1, 0, 1]:
+                            xmas_instances += find_xmas_string(input,
+                            x_stride = x_stride, y_stride = y_stride, 
+                            starting_x = letter_idx, starting_y = row_idx,
+                            str_to_find = "XMAS")
                     
     return xmas_instances
+
+
+def generate_answer_part2(input: str) -> int:
+    input = input.rstrip().split("\n")
+
+    x_mas_instances = 0
+    for row_idx, row in enumerate(input):
+        for letter_idx, _ in enumerate(row):
+            # Find top left - bottom right half of the cross, either direction
+            x_stride = 1
+            y_stride = 1
+            if find_xmas_string(input,
+                        x_stride = x_stride, y_stride = y_stride, 
+                        starting_x = letter_idx, starting_y = row_idx,
+                        str_to_find = "MAS") or find_xmas_string(input,
+                        x_stride = x_stride, y_stride = y_stride, 
+                        starting_x = letter_idx, starting_y = row_idx,
+                        str_to_find = "SAM"):
+                # Find top righ - bottom left half of the cross, either direction
+                x_stride = -1
+                y_stride = 1
+                starting_x = letter_idx + 2
+                if find_xmas_string(input,
+                            x_stride = x_stride, y_stride = y_stride, 
+                            starting_x = starting_x, starting_y = row_idx,
+                            str_to_find = "MAS") or find_xmas_string(input,
+                            x_stride = x_stride, y_stride = y_stride, 
+                            starting_x = starting_x, starting_y = row_idx,
+                            str_to_find = "SAM"):
+                    x_mas_instances += 1
+                    
+    return x_mas_instances
 
 
 def find_xmas_string(puzzle_input: list[str],
@@ -47,6 +79,14 @@ class Test(unittest.TestCase):
         expected_anser = 18
         self.assertEqual(expected_anser, predicted_answer)
 
+    
+    def test_with_provided_example_part2(self):
+        with open("inputs/input4_test.txt") as f:
+            data = f.read()
+        predicted_answer = generate_answer_part2(data)
+        expected_anser = 9
+        self.assertEqual(expected_anser, predicted_answer)
+
 
 if __name__ == "__main__":
     unittest.main(argv=[''], verbosity=3, exit=False)
@@ -56,6 +96,10 @@ if __name__ == "__main__":
 
     # Part 1
     answer = generate_answer(data)
+    print(f"Answer: {answer}")
+
+    # Part 2
+    answer = generate_answer_part2(data)
     print(f"Answer: {answer}")
 
 

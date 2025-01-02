91c250db9b3035448b596ab253b5ac49030ac31b
KasiaKozdon <katarzyna.kozdon@gmail.com>
2025-01-02 23:21:20 +0100

    Day 6, part 2

--- Day6.py
+++ Day6.py
@@ -1,3 +1,4 @@
+import typing
 import unittest
 
 
@@ -12,21 +13,56 @@ def generate_answer(data):
 
     nr_steps = 1
     marker_of_visit = 'x'
-    movement_history = mark_visit(movement_history, y_coord, x_coord, marker_of_visit)
+    movement_history = mark_visit_on_the_map(movement_history, y_coord, x_coord, marker_of_visit)
     while (0 <= x_coord < maze_width - 1) and (0 <= y_coord < maze_height - 1):
         next_x_coord = x_coord + x_heading
         next_y_coord = y_coord + y_heading
         if check_if_accessible(data, next_x_coord, next_y_coord):
             nr_steps += 1
-            movement_history = mark_visit(movement_history, y_coord, x_coord, marker_of_visit)
+            movement_history = mark_visit_on_the_map(movement_history, y_coord, x_coord, marker_of_visit)
             x_coord = next_x_coord
             y_coord = next_y_coord
         else:
             x_heading, y_heading = change_heading(x_heading, y_heading)
 
-    movement_history = mark_visit(movement_history, y_coord, x_coord, marker_of_visit)
+    movement_history = mark_visit_on_the_map(movement_history, y_coord, x_coord, marker_of_visit)
     nr_locations_visited = count_char_occurences(movement_history, marker_of_visit)
     return nr_locations_visited
+
+
+def generate_answer_part2(data):
+    data = data.strip("\n\n").split("\n")
+    maze_width = len(data[0])
+    maze_height = len(data)
+    start_x_coord, start_y_coord = find_location(data)
+    start_x_heading, start_y_heading = find_heading(data, start_x_coord, start_y_coord)
+
+    # Loop that places one obstruction at each location there is none
+    nr_infinite_loops = 0
+    for row_idx, row in enumerate(data):
+         for col_idx, letter in enumerate(row):
+            if (col_idx, row_idx) != (start_x_coord, start_y_coord):
+                if check_if_accessible(data, col_idx, row_idx):
+                    altered_map = add_obstacle_to_map(data, row_idx, col_idx)
+                    
+                    x_coord, y_coord = start_x_coord, start_y_coord
+                    x_heading, y_heading = start_x_heading, start_y_heading
+                    movement_history = [(y_coord, x_coord)]  
+
+                    while (0 <= x_coord < maze_width - 1) and (0 <= y_coord < maze_height - 1):
+                        next_x_coord = x_coord + x_heading
+                        next_y_coord = y_coord + y_heading
+                        if check_if_accessible(altered_map, next_x_coord, next_y_coord):
+                            x_coord = next_x_coord
+                            y_coord = next_y_coord
+                            movement_history.append((y_coord, x_coord))
+                            if detect_loop(movement_history):
+                                nr_infinite_loops += 1 
+                                break
+                        else:
+                            x_heading, y_heading = change_heading(x_heading, y_heading)
+
+    return nr_infinite_loops
 
 
 def find_location(maze: list[str]) -> tuple[int]:
@@ -65,9 +101,18 @@ def change_heading(x_heading: int, y_heading: int) -> tuple[int]:
     return (new_x_heading, new_y_heading)
 
 
-def mark_visit(movement_history: list[str], y_coord: int, x_coord: int, marker_of_visit: str):
-    movement_history[y_coord] = movement_history[y_coord][:x_coord] + marker_of_visit + movement_history[y_coord][x_coord+1:]
-    return movement_history
+def mark_visit_on_the_map(movement_history: list[str], y_coord: int, x_coord: int, marker_of_visit: str = "x"):
+    return generate_altered_map(movement_history, y_coord, x_coord, marker_of_visit)
+
+
+def add_obstacle_to_map(maze: list[str], y_coord: int, x_coord: int, marker_to_place: str = "#"):
+    return generate_altered_map(maze, y_coord, x_coord, marker_to_place)
+
+
+def generate_altered_map(map: list[str], y_coord: int, x_coord: int, marker_to_place: str):
+    new_map = map.copy()
+    new_map[y_coord] = new_map[y_coord][:x_coord] + marker_to_place + new_map[y_coord][x_coord+1:]
+    return new_map
 
 
 def count_char_occurences(maze: list[str], symbol_to_count: str = 'x') -> int:
@@ -76,6 +121,22 @@ def count_char_occurences(maze: list[str], symbol_to_count: str = 'x') -> int:
         occurences += row.count(symbol_to_count)
     return occurences
 
+
+def detect_loop(movement_history: list[tuple[int, int]]) -> bool:
+    # check if new location already exists
+    current_location = movement_history[-1]
+    previous_location = movement_history[-2]
+    other_locations = movement_history[:-2]
+
+    if current_location in other_locations:
+         if previous_location in other_locations:
+            for idx, location in enumerate(other_locations):
+                if location == previous_location:
+                    if other_locations[idx + 1] == current_location:
+                        return True
+                              
+    return False          
+
 class Test(unittest.TestCase):
     def test_with_provided_example(self):
         with open("inputs/input6_test.txt") as f:
@@ -83,7 +144,15 @@ class Test(unittest.TestCase):
 
         expected_answer = 41
         predicted_answer = generate_answer(data)
-        self.assertAlmostEqual(expected_answer, predicted_answer)
+        self.assertEqual(expected_answer, predicted_answer)
+
+    def test_with_provided_example_part2(self):
+        with open("inputs/input6_test.txt") as f:
+            data = f.read()
+
+        expected_answer = 6
+        predicted_answer = generate_answer_part2(data)
+        self.assertEqual(expected_answer, predicted_answer)
 
 
 if __name__ == "__main__":
@@ -95,3 +164,7 @@ if __name__ == "__main__":
     # Part 1
     answer = generate_answer(data)
     print(f"Answer for part 1: {answer}")
+
+    # Part 2
+    answer = generate_answer_part2(data)
+    print(f"Answer for part 2: {answer}")
